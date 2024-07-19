c36c793d3428411ee8f1bf80976c2aabb5988509
Kim Doha <96649881+esueng@users.noreply.github.com>
2024-07-19 09:50:17 +0900

    Add files via upload
    기반 파일 추가

---
+++ be08-2nd-AOSynergy-BORROW/src/test/java/com/beyond/kkwoborrow/KkwoborrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyond.kkwoborrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkwoborrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
